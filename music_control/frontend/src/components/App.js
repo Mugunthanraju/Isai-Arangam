d934c0514fbdb6ea8d6c51ef1d3918a26bea82f0
Mugunthan Raju <mugunthanraju2000@gmail.com>
2021-08-01 17:21:31 +0530

    Full stack app

---
+++ music_control/frontend/src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+// import HomePage from "./HomePage";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <h1>{this.props.name}</h1>;
+  }
+}
+
+const appDiv = document.getElementById("app");
+render(<App name="Hello" />, appDiv);
+
+ 
